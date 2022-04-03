83783944c13098c48016af472a6175d7b5792d90
Roman Kushnir <68236214+Romakush85@users.noreply.github.com>
2022-04-03 16:45:00 +0300

    Create NameEncoderDecoder.java

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,25 @@
+class NameEncoderDecoder {
+    public String encode(String name) {
+        String codeName = "NOTFORYOU" + 
+          name
+            .replace("e", "1")
+            .replace("u", "2")
+            .replace("i", "3")
+            .replace("o", "4")
+            .replace("a", "5")
+      + "YESNOTFORYOU";
+      return codeName;
+    }
+  
+    public String decode(String name) {
+        String decodeName = name
+            .replace("YESNOTFORYOU", "")
+            .replace("1", "e")
+            .replace("2", "u")
+            .replace("3", "i")
+            .replace("4", "o")
+            .replace("5", "a")
+            .replace("NOTFORYOU", "");
+            return decodeName;
+    }
+}
